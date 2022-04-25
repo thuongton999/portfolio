5feccc4b13164eb4c293133d22aca689626a8c63
thuongton999 <thuongton0809999@gmail.com>
2022-04-25 17:06:44 +0700

    remove reportWebVitals function

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.css';
 import { BrowserRouter } from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
 
 const Desktop = React.lazy(() => import('./component/Desktop'));
 const Mobile = React.lazy(() => import('./component/Mobile'));
@@ -35,8 +34,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
